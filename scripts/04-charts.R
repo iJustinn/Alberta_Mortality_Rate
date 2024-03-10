204441d091a536fdcef7b929cfea7553d7ebace6
iJustinn <ijustinzhong@gmail.com>
2024-03-10 17:34:28 -0400

    charts scripts updated

---
+++ scripts/04-charts.R
@@ -0,0 +1,35 @@
+#### Preamble ####
+# Purpose: 
+# Author: Ziheng Zhong
+# Date: 10 March 2024
+# Contact: ziheng.zhong@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: none
+
+
+
+#### Workspace setup ####
+# setup all libraries
+library(knitr)
+library(janitor)
+library(ggplot2)
+library(tidyverse)
+
+
+
+#### load data ####
+filtered_data <- read_csv("outputs/data/raw_data.csv")
+
+
+
+#### generate charts ####
+filtered_data |>
+  ggplot(aes(x = calendar_year, y = total_deaths, color = cause)) +
+  geom_line() +
+  theme_minimal() +
+  scale_color_brewer(palette = "Set1") +
+  labs(x = "Year", y = "Annual number of deaths in Alberta") +
+  facet_wrap(vars(cause), dir = "v", ncol = 1) +
+  theme(legend.position = "none")
+
+
