98f25c4ee2186b69878d45a530016edd6f70c138
iJustinn <ijustinzhong@gmail.com>
2024-03-10 23:21:44 -0400

    modeling script updated

--- scripts/03-modeling.R
+++ scripts/03-modeling.R
@@ -10,13 +10,54 @@
 
 #### Workspace setup ####
 # setup all libraries
-library(opendatatoronto)
-library(tidyverse)
-library(ggplot2)
-library(janitor)
-library(dplyr)
-library(readr)
 
 
 
-#### xxx ####
+#### load data ####
+filtered_data <- read_csv("outputs/data/raw_data.csv")
+
+
+
+#### poisson model ####
+cause_of_death_alberta_poisson <-
+  stan_glm(
+    total_deaths ~ cause,
+    data = filtered_data,
+    family = poisson(link = "log"),
+    seed = 123
+  )
+
+
+
+#### negative binomial model ####
+cause_of_death_alberta_neg_binomial <-
+  stan_glm(
+    total_deaths ~ cause,
+    data = filtered_data,
+    family = neg_binomial_2(link = "log"),
+    seed = 123
+  )
+
+
+
+#### modeling results ####
+name_mapping <- 
+  c("causeAll other forms of chronic ..."
+    = "other chronic",
+    "causeMalignant neoplasms of trac..."
+    = "neoplasms",
+    "causeOrganic dementia"
+    = "dementia",
+    "causeOther chronic obstructive p..."
+    = "obstructive pulmonary"
+  )
+
+modelsummary(
+  list(
+    "Poisson" = cause_of_death_alberta_poisson,
+    "Negative binomial" = cause_of_death_alberta_neg_binomial
+  ),
+  coef_map = name_mapping
+)
+
+
